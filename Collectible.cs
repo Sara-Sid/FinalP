def844ef502846ad5d47f1ee19d0b87a9c14bd68
Sara-Sid <sara.brown.2005@outlook.com>
2026-02-10 01:16:28 -0500

    Simplify Collectible class by removing rotation
    Removed rotation functionality and related code.

--- Collectible.cs
+++ Collectible.cs
@@ -2,25 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-// Script for collectible items (coins, stars, etc.)
-// Author: [Your Name]
-// Attach this to any sphere or object you want the player to collect
-// Resource: Created for Unity obstacle course project
-
 public class Collectible : MonoBehaviour
 {
 	// Points awarded when collected
 	public int pointValue = 10;
-
-	// Optional: Rotate the collectible to make it spin
-	public float rotateSpeed = 50f;
-
-	void Update()
-	{
-		// Make the collectible spin (looks cool!)
-		transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
-	}
 
 	// Called when player touches the collectible
 	void OnTriggerEnter(Collider other)
@@ -36,3 +21,4 @@ public class Collectible : MonoBehaviour
 		}
 	}
 }
+
